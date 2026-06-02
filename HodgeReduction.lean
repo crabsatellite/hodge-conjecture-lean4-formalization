3011c198295faa6a3e264fd81a215bb857ff6b2d
Alex <islicy@outlook.com>
2026-06-02 09:58:58 +0900

    R726: Split Cartan comparison into containment targets

--- HodgeReduction.lean
+++ HodgeReduction.lean
@@ -638,6 +638,7 @@ import HodgeReduction.HCGapL4.FrontC157_H8ResidualBoundaryCompactDualPrimitiveCo
 import HodgeReduction.HCGapL4.FrontC158_H8ResidualBoundaryCompactDualIndependence
 import HodgeReduction.HCGapL4.FrontC159_H8ResidualPaperCarrierStackIndependence
 import HodgeReduction.HCGapL4.FrontC160_H8ResidualCurrentCartanComparisonRoute
+import HodgeReduction.HCGapL4.FrontC161_H8ResidualPaperCarrierCartanContainmentIndependence
 -- import HodgeReduction.HCGapL4.R494_MultiFrontWave12Audit -- temporarily disabled for build fix
 -- import HodgeReduction.HCGapL4.FrontE9_MTCorrespondenceWitness -- temporarily disabled for build fix
 -- import HodgeReduction.HCGapL4.R496_MultiFrontWave13Audit -- temporarily disabled for build fix
